b05eff49c0b1ad324bd5b43b63bce992e8abccbb
linzexin <linzexinmasterchief@gmail.com>
2018-04-08 13:12:40 +0800

    update

--- AI_eca/tensorflowLearn/scripts/notes.js
+++ AI_eca/tensorflowLearn/scripts/notes.js
@@ -1,11 +1,11 @@
-$.getJSON("json/notes.json", function(data){
-  $.each(data.items, function(i,item){
-    $("<img/>").attr("src", item.media.m).appendTo("#images");
-    if ( i == 3 ) return false;
-  });
-});
-
 $(document).ready(function(){
+	$.getJSON("json/notes.json", function(data){
+		$.each(data.items, function(i,item){
+			obj = JSON.parse(item);
+			document.getElementById("testfield").innerHTML = obj.name + " " + obj.sex;
+		});
+	});
+
 	$("button").click(function(){
 		id = "#" + $(this).attr('id').slice(0, -6);
 		$(id).slideToggle();
